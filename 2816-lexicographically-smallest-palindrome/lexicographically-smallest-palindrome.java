06b7ac6ff51eb0812a4db7597f3160fbea1273f5
Bikram Dey <bikramdey458@gmail.com>
2024-03-13 08:59:39 +0530

    Time: 7 ms (66.35%) | Memory: 44.9 MB (92.83%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -1,9 +1,10 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
         char[] ch = s.toCharArray();
-        for(int i=0 , j=ch.length - 1; i<j; i++, j--){
-            if (ch[i] != ch[j]) {
-                ch[i] = ch[j] = ch[i] < ch[j] ? ch[i] : ch[j];
+        int n = s.length();
+        for(int i=0; i<n/2; i++){
+            if (ch[i] != ch[n-i-1]) {
+                ch[i] = ch[n-i-1] = ch[i] < ch[n-i-1] ? ch[i] : ch[n-i-1];
             }
         }
         return String.valueOf(ch);
