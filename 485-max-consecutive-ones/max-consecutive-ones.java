b9ee79ec2728514c4daab9243cafa7f43cfc00c4
Bikram Dey <bikramdey458@gmail.com>
2024-03-21 09:21:19 +0530

    Time: 3 ms (47.95%) | Memory: 45.3 MB (72.22%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -3,11 +3,12 @@ class Solution {
         int ans = 0;
         int len = 0;
 
-        for (final int num : nums)
-        if (num == 1)
-            ans = Math.max(ans, ++len);
-        else
-            len = 0;
+        for (final int num : nums){
+            if (num == 1)
+                ans = Math.max(ans, ++len);
+            else
+                len = 0;
+        }
 
         return ans;
     }
