d8a597547dde27b7eb5e0f0be93191b4963008eb
Bikram Dey <bikramdey458@gmail.com>
2024-03-26 11:27:33 +0530

    Time: 8 ms (87.96%) | Memory: 46.3 MB (78.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,39 +1,21 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0])); //O(nlogn)
-           Stack<int[]> stack = new Stack(); //O(n)
-           stack.add(intervals[0]);
-        
-        
-        for(int i=1;i<intervals.length;i++){
-            
-            int startpoint2 = intervals[i][0];
-            int endpoint2 = intervals[i][1];
-            
-            int[] poparray = stack.pop();
-            
-            int startpoint1 = poparray[0];
-            int endpoint1 = poparray[1];
-            
-            int endmax = Math.max(endpoint2,endpoint1);
-                
-            if(endpoint1>=startpoint2){
-                int[] merge = new int[]{startpoint1,endmax};
-                stack.add(merge);
+    public int[][] merge(int[][] arr) {
+        int n = arr.length;
+        Arrays.sort(arr,(a,b)->(a[0]-b[0]));
+        ArrayList<int[]> a = new ArrayList<>();
+        a.add(arr[0]);
+        for(int i=1; i<n; i++){
+            int x[] = a.get(a.size()-1);
+            if(arr[i][0]>x[1]){
+                a.add(arr[i]);
             }else{
-                stack.add(poparray);
-                stack.add(intervals[i]);
+                a.get(a.size()-1)[1] = Math.max(x[1],arr[i][1]);
             }
-             
         }
-        
-        int[][] output = new int[stack.size()][2];
-        
-        for(int i=output.length-1;i>=0;i--){
-            int[] poparray = stack.pop();
-            output[i][0] = poparray[0];
-            output[i][1] = poparray[1];
+        int ans[][] = new int[a.size()][2];
+        for(int i=0; i<a.size(); i++){
+            ans[i] = a.get(i);
         }
-        return output;
+        return ans;
     }
 }
