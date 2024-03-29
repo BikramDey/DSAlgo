b9a047a198926c1cead4b8aad1bbe1ed74a476da
Bikram Dey <bikramdey458@gmail.com>
2024-03-29 13:24:05 +0530

    Time: 1133 ms (10.07%) | Memory: 66.8 MB (35.77%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,37 +1,23 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // Create a hash set to store the unique elements of the array.
-        Set<Integer> numSet = new HashSet<>();
-      
-        // Add all elements to the set.
+        if(nums == null || nums.length == 0) 
+            return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums) 
+            set.add(n);
+
+        int result = 0;
+
         for (int num : nums) {
-            numSet.add(num);
+            if (set.contains(num - 1))
+                continue;
+            int length = 1;
+            while (set.contains(++num))
+                ++length;
+            result = Math.max(result, length);
         }
-      
-        // Initialize the variable for the longest consecutive sequence.
-        int longestStreak = 0;
-      
-        // Go through each element in the array.
-        for (int num : nums) {
-            // Check if current number is the beginning of a sequence.
-            if (!numSet.contains(num - 1)) {
-                // Initialize the current number as the potential start of the sequence.
-                int currentNum = num;
-                // Initialize the current streak length.
-                int currentStreak = 1;
-              
-                // Expand the current streak if consecutive numbers are found.
-                while (numSet.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-              
-                // Update the longest streak found so far.
-                longestStreak = Math.max(longestStreak, currentStreak);
-            }
-        }
-      
-        // Return the longest streak length.
-        return longestStreak;
+
+        return result;
     }
 }
