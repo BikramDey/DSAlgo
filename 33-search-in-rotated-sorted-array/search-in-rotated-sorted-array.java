6ad389dc71b6c3c6b652de6ca972eb5949b7718c
Bikram Dey <bikramdey458@gmail.com>
2024-04-08 22:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (76.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,46 +1,25 @@
 class Solution {
-    public int search(int[] a, int target) {
-        
-        int n=a.length;
-        int p=pivotIndex(a, 0, n - 1);
-        int z=-1;
-        if(p==-1){
-            z = bsearch(a,0,n-1,target);
-        }
-        else{
-            int x = bsearch(a,0,p,target);
-            int y = bsearch(a,p+1,n-1,target);
-            z = Math.max(x,y);
-        }
-        return z;
-    }
-    static int pivotIndex(int arr[], int l, int r)
-    {
-        if (l>r)
-            return -1;
-        if (l==r)
-            return l;
- 
-        int mid = (l + r)/2;
-        if (mid < r && arr[mid] > arr[mid + 1])
-            return mid;
-        if (mid > l && arr[mid] < arr[mid - 1])
-            return (mid - 1);
-        if (arr[l] >= arr[mid])
-            return pivotIndex(arr, l, mid - 1);
-        return pivotIndex(arr, mid + 1, r);
-    }
-    public static int bsearch(int a[], int l, int r, int k){
-        if(l>r){
-            return -1;
-        }
-        int mid=(l+r)/2;
-        if(a[mid]==k)
-            return mid;
-        
-        if(a[mid]<k)
-            return bsearch(a,mid+1,r,k);
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
 
-        return bsearch(a,l,mid-1,k);
+        while (l <= r) {
+        final int m = l + (r - l) / 2;
+        if (nums[m] == target)
+            return m;
+        if (nums[l] <= nums[m]) {
+            if (nums[l] <= target && target < nums[m])
+            r = m - 1;
+            else
+            l = m + 1;
+        } else {
+            if (nums[m] < target && target <= nums[r])
+            l = m + 1;
+            else
+            r = m - 1;
+        }
+        }
+
+        return -1;
     }
 }
