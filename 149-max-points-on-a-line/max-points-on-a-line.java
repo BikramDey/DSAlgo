4ea8214f6bae854dbd7c87db933c85fa0e9ca1e3
Bikram Dey <bikramdey458@gmail.com>
2024-04-09 22:00:02 +0530

    Time: 23 ms (68.54%) | Memory: 45.1 MB (14.24%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -8,14 +8,14 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             HashMap<Double, Integer> slopeMap = new HashMap<>();
-            int max=0;
+            int max = 0;
             for (int j = i + 1; j < n; j++) {
-                int num=(points[j][0]-points[i][0]);
-                int den=(points[j][1]-points[i][1]);
+                int num = points[j][0]-points[i][0];
+                int den = points[j][1]-points[i][1];
                 double slope = (den==0)? Double.POSITIVE_INFINITY:(double)num/den;
 
-                if(slope==0) 
-                    slope=0;
+                if(slope == 0) 
+                    slope = 0;
                     
                 slopeMap.put(slope, slopeMap.getOrDefault(slope, 0) + 1);
                 max = Math.max(slopeMap.get(slope),max);
