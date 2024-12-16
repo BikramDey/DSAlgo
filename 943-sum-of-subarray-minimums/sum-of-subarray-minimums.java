41e75ac3344caf0ccd31e8451fc23c0c30844a8e
Bikram Dey <bikramdey458@gmail.com>
2024-12-16 19:43:51 +0530

    Time: 39 ms (70.67%) | Memory: 49.3 MB (87.51%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,39 +1,28 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int kMod = 1_000_000_007;
+        int m = 1000000007;
         int n = arr.length;
         long ans = 0;
         int[] prevMin = new int[n];
         int[] nextMin = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
+        Stack<Integer> st = new Stack<>();
         Arrays.fill(prevMin, -1);
         Arrays.fill(nextMin, n);
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
+        for(int i = 0; i<n; i++){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                int ind = st.pop();
+                nextMin[ind] = i;
             }
-            if (!stack.isEmpty())
-                prevMin[i] = stack.peek();
-
-            stack.push(i);
-        }
-        stack.clear();
-        for (int i = n-1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+            if(!st.isEmpty()){
+                prevMin[i] = st.peek();
             }
-            if (!stack.isEmpty())
-                nextMin[i] = stack.peek();
-
-            stack.push(i);
+            st.push(i);
         }
-        for (int i = 0; i < n; ++i) {
-            ans += (long) arr[i] * (i - prevMin[i]) * (nextMin[i] - i);
-            ans %= kMod;
+        st.clear();
+        for(int i = 0; i<n; i++){
+            ans += (long)(i - prevMin[i]) * (nextMin[i] - i) % m * arr[i] % m;
+            ans %= m;
         }
-
-        return (int) ans;
-
+        return (int)ans;
     }
 }
