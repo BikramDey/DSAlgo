c999770294bffbe252ff1c0c31a4bcce5cd622a2
Bikram Dey <bikramdey458@gmail.com>
2025-08-23 20:15:49 +0530

    Time: 3 ms (91.69%) | Memory: 44.3 MB (33.90%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int l1 = nums1.length, l2 = nums2.length;
+        Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = nums2.length-1; i>=0; i--){
-            while(!stack.isEmpty() && nums2[i]>=stack.peek()){
+        map.put(nums2[l2-1], -1);
+        stack.push(nums2[l2-1]);
+        for(int i = l2-2; i>=0;i--){
+            while(!stack.isEmpty() && nums2[i]>stack.peek()){
                 stack.pop();
             }
-            if(!stack.isEmpty()) {
+            if(stack.isEmpty()){
+                map.put(nums2[i], -1);
+            }else{
                 map.put(nums2[i], stack.peek());
             }
             stack.push(nums2[i]);
         }
-        int n = nums1.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; ++i) {
-            result[i] = map.getOrDefault(nums1[i], -1);
+        for(int i = l1-1; i>=0;i--){
+            nums1[i] = map.get(nums1[i]);
         }
-        return result;
+        return nums1;
+
     }
 }
