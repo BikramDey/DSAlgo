55c7394ebcaa4ed980eb3630ea0dd33bc974de0e
Bikram Dey <bikramdey458@gmail.com>
2026-03-12 07:45:26 +0000

    Time: 7 ms (9.48%) | Memory: 46.5 MB (95.93%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,16 +14,16 @@ public class Solution {
         if(head == null || head.next == null)
             return false;
 
-        ListNode slow = head;
-        ListNode fast = head;
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = head;
 
-        while(slow!=null && fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast)
+        while(temp!=null){
+            if(set.contains(temp))
                 return true;
+            
+            set.add(temp);
+            temp = temp.next;
         }
         return false;
-        
     }
 }
