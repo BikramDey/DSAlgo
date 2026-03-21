4a3aacaea8e1040e9a1989cc0f89b7e020f71809
Bikram Dey <bikramdey458@gmail.com>
2026-03-21 13:28:26 +0000

    Time: 2 ms (99.84%) | Memory: 43.4 MB (81.95%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -2,20 +2,54 @@ class Solution {
     public String removeOuterParentheses(String s) {
         if(s.length() == 0)
             return "";
+        // String Approach
+        // int l = s.length();
+        // int index = 0, c = 0;
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0; i<l; i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == '(')
+        //         c++;
+        //     if(ch == ')')
+        //         c--;
 
-        int l = s.length();
-        int index = 0, c = 0;
+        //     if(c==0){
+        //         sb.append(s.substring(index+1, i));
+        //         index = i+1;
+        //     }
+        // }
+        // return sb.toString();
+
+        // Stack Approach
+        // Stack<Character> stack = new Stack<>();
+        // StringBuilder sb = new StringBuilder();
+        // for(char ch : s.toCharArray()){
+        //     if(ch == '('){
+        //         stack.push(ch);
+        //         if (stack.size() > 1) 
+        //             sb.append('(');
+        //     }
+        //     if(ch == ')'){
+        //         if (stack.size() > 1) 
+        //             sb.append(')');
+        //         stack.pop();
+        //     }
+        // }
+        // return sb.toString();
+
+        // More Simpler Stack Approach
+        int c = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<l; i++){
-            char ch = s.charAt(i);
-            if(ch == '(')
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
                 c++;
-            if(ch == ')')
+                if (c > 1) 
+                    sb.append('(');
+            }
+            if(ch == ')'){
+                if (c > 1) 
+                    sb.append(')');
                 c--;
-
-            if(c==0){
-                sb.append(s.substring(index+1, i));
-                index = i+1;
             }
         }
         return sb.toString();
