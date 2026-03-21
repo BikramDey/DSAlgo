580aa217072f745499bb3b853fa28c4de1b66a27
Bikram Dey <bikramdey458@gmail.com>
2026-03-21 15:51:50 +0000

    Time: 8 ms (12.80%) | Memory: 45.4 MB (40.36%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,34 +1,53 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> st = new Stack<>();
-        for(String str : tokens){
-            if(str.equals("+")){
-                int a = Integer.valueOf(st.pop());
-                int b = Integer.valueOf(st.pop());
-                int c = b + a;
-                st.push(String.valueOf(c));
-            }
-            else if(str.equals("-")){
-                int a = Integer.valueOf(st.pop());
-                int b = Integer.valueOf(st.pop());
-                int c = b - a;
-                st.push(String.valueOf(c));
-            }
-            else if(str.equals("*")){
-                int a = Integer.valueOf(st.pop());
-                int b = Integer.valueOf(st.pop());
-                int c = b * a;
-                st.push(String.valueOf(c));
-            }
-            else if(str.equals("/")){
-                int a = Integer.valueOf(st.pop());
-                int b = Integer.valueOf(st.pop());
-                int c = b / a;
-                st.push(String.valueOf(c));
-            }else{
-                st.push(str);
-            }
+        // Stack<String> st = new Stack<>();
+        // for(String str : tokens){
+        //     if(str.equals("+")){
+        //         int a = Integer.valueOf(st.pop());
+        //         int b = Integer.valueOf(st.pop());
+        //         int c = b + a;
+        //         st.push(String.valueOf(c));
+        //     }
+        //     else if(str.equals("-")){
+        //         int a = Integer.valueOf(st.pop());
+        //         int b = Integer.valueOf(st.pop());
+        //         int c = b - a;
+        //         st.push(String.valueOf(c));
+        //     }
+        //     else if(str.equals("*")){
+        //         int a = Integer.valueOf(st.pop());
+        //         int b = Integer.valueOf(st.pop());
+        //         int c = b * a;
+        //         st.push(String.valueOf(c));
+        //     }
+        //     else if(str.equals("/")){
+        //         int a = Integer.valueOf(st.pop());
+        //         int b = Integer.valueOf(st.pop());
+        //         int c = b / a;
+        //         st.push(String.valueOf(c));
+        //     }else{
+        //         st.push(str);
+        //     }
+        // }
+        // return Integer.valueOf(st.pop());
+
+
+        Map<String, BinaryOperator<Integer>> op = Map.of(
+        "+", (a, b) -> a + b, 
+        "-", (a, b) -> a - b, 
+        "*", (a, b) -> a * b, 
+        "/", (a, b) -> a / b);
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens)
+        if (op.containsKey(token)) {
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(op.get(token).apply(a, b));
+        } else {
+            stack.push(Integer.valueOf(token));
         }
-        return Integer.valueOf(st.pop());
+
+        return Integer.valueOf(stack.pop());
     }
 }
