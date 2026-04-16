a91f12292ea545fc73d477ba7a7f9f22f5611b25
Bikram Dey <bikramdey458@gmail.com>
2026-04-16 10:21:21 +0000

    Time: 0 ms (100.00%) | Memory: 43.4 MB (86.96%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,29 +1,31 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root.left == null && root.right == null)
+        if(root.left == null && root.right==null)
             return true;
-        return checkBothHalf(root.left, root.right);
+        return checkSymmetric(root.left, root.right);
     }
+    public boolean checkSymmetric(TreeNode n1, TreeNode n2) {
+        if(n1 == null && n2 == null)
+            return true;
+        if((n1 == null && n2 != null )|| (n1 != null && n2 == null )|| n1.val != n2.val)
+            return false;
 
-    public static boolean checkBothHalf(TreeNode leftside, TreeNode rightside) {
-        if (leftside == null || rightside == null)
-            return leftside == rightside;
-        return (leftside.val == rightside.val && checkBothHalf(leftside.right, rightside.left)
-                && checkBothHalf(leftside.left, rightside.right));
+        return checkSymmetric(n1.left, n2.right) && checkSymmetric(n1.right, n2.left);
+        
     }
 }
