e75153bbd9e7f2e625d8843c1375d3d27ea7edea
Bikram Dey <bikramdey458@gmail.com>
2026-04-21 14:20:59 +0000

    Time: 0 ms (100.00%) | Memory: 46.6 MB (20.53%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -27,25 +27,39 @@ class Solution {
         if(root == null)
             return root;
             
-        Deque<Node> q = new LinkedList<>();
-        q.add(root);
-        root.next = null;
-        while(!q.isEmpty()){
-            int s = q.size();
-            while(s>0){
-                Node t = q.poll();
-                t.next = q.peek();
-                if(s==1)
-                    t.next = null;
+        // Deque<Node> q = new LinkedList<>();
+        // q.add(root);
+        // root.next = null;
+        // while(!q.isEmpty()){
+        //     int s = q.size();
+        //     while(s>0){
+        //         Node t = q.poll();
+        //         t.next = q.peek();
+        //         if(s==1)
+        //             t.next = null;
                     
-                if (t.left != null)
-                    q.add(t.left);
-                if (t.right != null)
-                    q.add(t.right);
+        //         if (t.left != null)
+        //             q.add(t.left);
+        //         if (t.right != null)
+        //             q.add(t.right);
 
-                s--;
-            }
-        }
+        //         s--;
+        //     }
+        // }
+        // return root;
+        traversal(root);
         return root;
+    }
+    public void traversal(Node curr) {
+        if(curr == null)
+            return;
+        if (curr.left != null) {
+            curr.left.next = curr.right;
+        }
+        if (curr.right != null && curr.next != null) {
+            curr.right.next = curr.next.left;
+        }
+        if (curr.left != null) traversal(curr.left);
+        if (curr.right != null) traversal(curr.right);
     }
 }
