fd64311a365edf94b5df6f00c95f786df76c6689
Bikram Dey <bikramdey458@gmail.com>
2026-05-10 13:03:26 +0000

    Time: 8 ms (91.20%) | Memory: 49.4 MB (13.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        ArrayList<int[]> arr = new ArrayList<>();
-        for(int i = 0; i<n; i++){
-            arr.add(intervals[i]);
-        }
-        Collections.sort(arr, (a,b) -> (a[0] - b[0]));
-        //int s = arr.size();
-        for(int i = 1; i<arr.size(); i++){
-            int current[] = arr.get(i);
-            int last[] = arr.get(i-1);
-            if(current[0] <= last[1]){
-                last[1] = Math.max(current[1], last[1]);
-                arr.remove(i);
-                i--;
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        ArrayList<int[]> ans = new ArrayList<>();
+        ans.add(intervals[0]);
+
+        // You cannot do this way as ans.get and update
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= ans.get(ans.size() - 1)[1]) {
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
+            } else {
+                ans.add(intervals[i]);
             }
         }
-        int newintervals[][] = new int[arr.size()][2];
-        int j = 0;
-        for(int item[] : arr){
-            newintervals[j][0] = item[0];
-            newintervals[j][1] = item[1];
-            j++;
-        }
-        return newintervals;
+
+        return ans.toArray(new int[ans.size()][]);
     }
 }
